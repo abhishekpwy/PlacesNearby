cfbd15aa182d83d6466d2b209b884ce8b724a2dc
Abhishek Kumar Pandey <abhishek.pandey@webyog.com>
2017-02-02 23:47:37 +0530

    Fixed reachability

--- PlacesNearby/PNBNetwrokConnectivity.swift
+++ PlacesNearby/PNBNetwrokConnectivity.swift
@@ -13,7 +13,7 @@ public class Reachability {
 
 	class func isConnectedToNetwork() -> Bool {
 
-		var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
+		var zeroAddress = sockaddr_in()
 		zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
 		zeroAddress.sin_family = sa_family_t(AF_INET)
 
@@ -23,15 +23,13 @@ public class Reachability {
 			}
 		}
 
-		var flags: SCNetworkReachabilityFlags = SCNetworkReachabilityFlags(rawValue: 0)
-		if SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) == false {
+		var flags = SCNetworkReachabilityFlags()
+		if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
 			return false
 		}
-
-		let isReachable = flags == .reachable
-		let needsConnection = flags == .connectionRequired
-
-		return isReachable && !needsConnection
+		let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+		let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+		return (isReachable && !needsConnection)
 
 	}
 }
